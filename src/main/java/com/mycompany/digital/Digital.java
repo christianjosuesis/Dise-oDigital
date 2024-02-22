efb14634473888edc1f055180b8955bb163741f8
chris <chris@192.168.0.158>
2024-02-22 09:20:02 -0600

    Se integran cambios para el proyecto, clase main y configuraciones

---
+++ src/main/java/com/mycompany/digital/Digital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.digital;
+
+/**
+ *
+ * @author chris
+ */
+public class Digital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
