97beb82af6175658f75c9f9c0ee36c7a7b372fd4
christianjosuesis <160763348+christianjosuesis@users.noreply.github.com>
2024-02-22 09:30:42 -0600

    Update Digital.java
    update hola mundo

--- src/main/java/com/mycompany/digital/Digital.java
+++ src/main/java/com/mycompany/digital/Digital.java
@@ -11,6 +11,6 @@ package com.mycompany.digital;
 public class Digital {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola desde el portal de github!");
     }
 }
